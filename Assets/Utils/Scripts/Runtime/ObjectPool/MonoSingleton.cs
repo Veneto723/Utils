097e446b0033d00a587505a0c6e738bd6ff3fe4e
John Yan <johnyan723@outlook.com>
2022-05-12 23:59:37 -0500

    Update to 1.0.5
    Finalize DigitController.cs

--- Assets/Utils/Scripts/Runtime/ObjectPool/MonoSingleton.cs
+++ Assets/Utils/Scripts/Runtime/ObjectPool/MonoSingleton.cs
@@ -3,10 +3,9 @@ using UnityEngine;
 namespace Utils.Scripts.Runtime.ObjectPool {
     public class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T> {
         public static T Instance { get; private set; }
-
+        
+        
         protected virtual void Awake() {
-            // if (Instance != null) Debug.Log($"{name} has already benn initialized!");
-
             Instance = (T) this;
         }
     }
